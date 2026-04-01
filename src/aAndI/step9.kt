1da1d2d472c836e8591a5396ba23eeb65b28aa24
Reo <pokk123123a@gmail.com>
2026-04-01 17:04:21 +0900

    2주차 9 미완성

--- src/aAndI/step9.kt
+++ src/aAndI/step9.kt
@@ -1,2 +1,26 @@
 package aAndI.p9
 
+fun main() {
+
+}
+
+
+fun solution(a: Int, b: Int): String {
+    
+    var min = b
+    var hour = a
+    
+    if (b >= 45) {
+        min -= 45
+    }
+    else if (b < 45) {
+        min += 15
+        hour -= 1
+        
+        if (hour < 0) {
+            hour += 24
+        }
+    }
+    val answer = "$hour $min"
+    return answer
+}
