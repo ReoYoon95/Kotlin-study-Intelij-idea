c9d08c2d7f2a06f5474ca4ac35d3e4b5978ffce6
Reo <pokk123123a@gmail.com>
2026-04-01 16:00:35 +0900

    2주차 8 커밋

---
+++ src/aAndI/step8.kt
@@ -0,0 +1,13 @@
+package aAndI.p8
+
+fun main() {
+    val inputA = readln().removePrefix("a = ").trim().toInt()
+    println(solution(inputA))
+
+}
+
+fun solution(a: Int): Int {
+    val result = if (a % 4 == 0 && (a % 100 != 0 || a % 400 == 0)) 1
+        else 0
+    return result
+}
