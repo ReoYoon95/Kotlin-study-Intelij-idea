e5fc28c76d3e9786a6011610e2fef4c7de6409f3
Reo <pokk123123a@gmail.com>
2026-04-01 18:42:24 +0900

    2주차과제 9

--- src/aAndI/step9.kt
+++ src/aAndI/step9.kt
@@ -1,22 +1,24 @@
 package aAndI.p9
 
 fun main() {
+    val inputA = readln().removePrefix("a = ").trim().toInt()
+    val inputB = readln().removePrefix("b = ").trim().toInt()
+    println(solution(inputA, inputB))
 
 }
 
 
 fun solution(a: Int, b: Int): String {
-    
+
     var min = b
     var hour = a
-    
+
     if (b >= 45) {
         min -= 45
-    }
-    else if (b < 45) {
+    } else if (b < 45) {
         min += 15
         hour -= 1
-        
+
         if (hour < 0) {
             hour += 24
         }
