8c9f7b9ce59627858d9a4595c022d74252cdceea
Reo <pokk123123a@gmail.com>
2026-04-01 20:32:52 +0900

    2주차과제 10 최종수정

--- src/aAndI/step10.kt
+++ src/aAndI/step10.kt
@@ -7,22 +7,23 @@ fun main() {
 }
 
 fun solution(a: String): String {
-    val result = a.trim()
+    val result = a.removePrefix("a = ").trim()
     val answer: String
 
-    if (result[0] == '/') answer = "UNIX_ABSOLUTE"
-    else if (result.length >= 3 && result[0].isLetter() && result[1] == ':' && result[2] == '\\') answer =
-        "WINDOWS_ABSOLUTE"
-    else answer = "RELATIVE"
+    if (result.isEmpty()) return "RELATIVE"
+
+    answer = if (result[0] == '/') "UNIX_ABSOLUTE"
+    else if (result.length >= 3 && result[0] in 'A'..'Z' && result[1] == ':' && result[2] == '\\') "WINDOWS_ABSOLUTE"
+    else "RELATIVE"
 
     return answer
 }
 
 fun solution1(a: String): String {
-    val result = a.trim()
+    val result = a.removePrefix("a = ").trim()
     return when {
         result.startsWith("/") -> "UNIX_ABSOLUTE"
-        result.length >= 3 && result[0].isLetter() && result[1] == ':' && result[2] == '\\' -> "WINDOWS_ABSOLUTE"
+        result.length >= 3 && result[0].isLetter() && result[1] == ':' -> "WINDOWS_ABSOLUTE"
         else -> "RELATIVE"
     }
 }
