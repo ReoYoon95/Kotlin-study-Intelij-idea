15b6f9a79284b4e3cfec69fa26977befdfb765b4
Reo <pokk123123a@gmail.com>
2026-04-01 13:04:08 +0900

    2주차과제 7

---
+++ src/aAndI/step7.kt
@@ -0,0 +1,30 @@
+package aAndI.p7
+
+fun main() {
+    val inputA = readln().removePrefix("a = ").trim().toInt()
+    val inputB = readln().removePrefix("a = ").trim().toInt()
+    println(solution(inputA, inputB))
+}
+
+fun solution(a: Int, b: Int): String {
+    val result = when {
+        a > b -> ">"
+        a < b -> "<"
+        else -> "=="
+    }
+    return result
+}
+
+fun solution1(a: Int, b: Int): String {
+    val result =
+        if (a > b) ">"
+        else if (a < b) "<"
+        else "=="
+    return result
+}
+
+fun solution2(a: Int, b: Int): String {
+    return if (a > b) ">" //아니면 return 을 바로 써도 됌
+    else if (a < b) "<"
+    else "=="
+}
