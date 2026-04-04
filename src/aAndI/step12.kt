a6d4f95be6b7904228d2273d602a20dd096161f3
Reo <pokk123123a@gmail.com>
2026-04-04 17:02:03 +0900

    260404 공부한 것 저일 +12번 수정

--- src/aAndI/step12.kt
+++ src/aAndI/step12.kt
@@ -10,12 +10,23 @@ fun solution(branchName: String): Boolean {
     val inputA = branchName.removePrefix("a = ").trim()
 
     val answer: Boolean = when {
-        inputA.startsWith("feature/") && inputA.length > "feature/".length -> true
+        inputA.startsWith("feature/") && inputA.length > "feature/".length && inputA.length - 1 = 'A' .. 'z' -> true
         inputA.startsWith("fix/") && inputA.length > "fix/".length -> true
         inputA.startsWith("hotfix/") && inputA.length > "hotfix/".length -> true
         inputA.startsWith("release/") && inputA.length > "release/".length -> true
         else -> false
     }
+
+    return answer
+}
+
+fun solution1(branchName: String): Boolean {
+    val inputA = branchName.removePrefix("a = ").trim()
+
+    if (inputA.startswith("feature/") && "feature/".length() > )
+    }
+
+
 
     return answer
 }
---
+++ src/study/260404study.kt
@@ -0,0 +1,46 @@
+package study
+
+fun main() {
+//    val result = sum(3, 5)
+//    println(result)
+//    printSum(4,6)
+    var inputA = readln().trim()
+    var inputV = readln().trim()
+    println(solution1(listOf(1, 4, 1, 2, 4, 2, 4, 2, 3, 4, 4), 2)
+
+}
+
+fun sum(a : Int, b : Int) : Int {
+    return a + b
+}
+
+fun printSum(a: Int, b: Int) {
+    println("합계는 ${a + b}")
+}
+
+fun gugudan(a : Int): String {
+    for (i in 2 .. 9) {
+       println( "${a * i}\t")
+    if (i % 3 == 0) println("")
+    }
+}
+
+fun gugudan1(a : Int): String {
+    var result :String = ""
+    for (i in 2..9) {
+        result += ("$a * $i = ${a * i}\n")}
+    return result
+}
+
+fun solution1(a: List<Int>, v: Int): Int {
+    var answer = 0
+    for (i in 0 .. a.size -1 ) {
+        if( a[i] == v) answer++
+    }
+    return answer
+}
+
+fun solution2(a: String, b: Int): Char {
+    var answer = a[b - 1]
+    return answer
+}
