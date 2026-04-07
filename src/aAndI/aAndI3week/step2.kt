ec5b257fdfc9f16f5cd613c7567a72b13902ae78
Reo <pokk123123a@gmail.com>
2026-04-07 21:38:14 +0900

    3주차 2단계문제

--- src/aAndI/aAndI3week/step2.kt
+++ src/aAndI/aAndI3week/step2.kt
@@ -1,10 +1,24 @@
 package aAndI.aAndI3week
 
 fun main() {
+    println("배열을 입력하시오")
+    val inputA = readln()
+        .substringAfter("[")    // "[" 뒤만 남겨라 -> "1,4,1,2,4,2,4,2,3,4,4]"
+        .substringBefore("]")   // "]" 앞만 남겨라 -> "1,4,1,2,4,2,4,2,3,4,4"
+        .split(",")             // 쉼표로 쪼개라 -> ["1", "4", "1", ...] (문자열 리스트)
+        .map { it.trim().toInt() } // 숫자로 바꿔라 -> [1, 4, 1, ...] (정수 리스트)
+    println("찾을 숫자를 입력하시오.")
+    val inputB = readln().removePrefix("b = ").trim().toInt()
+
+    println(solution(inputA, inputB))
 
 }
 
-fun solution(list_N: List<Int>, v: Int): Int {
-    var answer = 0
-    return answer
+fun solution(a: List<Int>, b: Int): Int {
+    var count = 0
+    for (num in a) {
+        if (b == num) count++
+    }
+    return count
 }
+
