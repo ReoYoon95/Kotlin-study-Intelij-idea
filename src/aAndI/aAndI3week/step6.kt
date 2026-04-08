3798240f1a596fa71a551bd37534183723758422
Reo <pokk123123a@gmail.com>
2026-04-08 15:50:51 +0900

    3주차 6단계 주석추가

--- src/aAndI/aAndI3week/step6.kt
+++ src/aAndI/aAndI3week/step6.kt
@@ -10,5 +10,7 @@ fun solution(S: String): String {
     if (S.isEmpty()) return ""
     val result = StringBuilder()
     result.append("${S[0]}${S[S.length - 1]}")
+//    마지막 인덱스를 부르는 방법은 [length -1] or [.lastIndex]를 사용한다.
+//    코틀린에서는 [-1]으로 마지막 인덱스 불러올 수 없음.
     return result.trim().toString()
 }
